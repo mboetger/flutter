c8c5a82a78c1634b8faa918ed23cc546f552ad55
louisehsu <louiseh0313@gmail.com>
2025-07-17 15:24:38 -0700

    fixed up constraint checking!

--- engine/src/flutter/shell/common/shell.h
+++ engine/src/flutter/shell/common/shell.h
@@ -46,10 +46,10 @@
 namespace flutter {
 
 typedef struct {
-    double min_width = 0.0;
-    double max_width = 0.0;
-    double min_height = 0.0;
-    double max_height = 0.0;
+  double min_width = 0.0;
+  double max_width = 0.0;
+  double min_height = 0.0;
+  double max_height = 0.0;
 } SizeConstraints;
 
 /// Error exit codes for the Dart isolate.
@@ -524,8 +524,6 @@ class Shell final : public PlatformView::Delegate,
 
   // used to discard wrong size layer tree produced during interactive
   // resizing
-  std::unordered_map<int64_t, DlISize> expected_frame_sizes_;
-
   std::unordered_map<int64_t, SizeConstraints> expected_frame_constraints_;
 
   // Used to communicate the right frame bounds via service protocol.
@@ -812,7 +810,8 @@ class Shell final : public PlatformView::Delegate,
 
   SizeConstraints ExpectedFrameSize(int64_t view_id);
 
-  bool isSizeWithinConstraints(const DlISize& size, const SizeConstraints& constraints);
+  bool isSizeWithinConstraints(const DlISize& size,
+                               const SizeConstraints& constraints);
 
   // For accessing the Shell via the raster thread, necessary for various
   // rasterizer callbacks.
