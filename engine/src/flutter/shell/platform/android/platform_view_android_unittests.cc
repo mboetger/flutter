3dca7de247e404b271a2adee882fe0ea1c00f7a8
Matt Boetger <boetger@google.com>
2026-08-25 15:34:55 -0700

    Hardened Android shell holder and rendering API unit tests for embedder migration baseline

--- engine/src/flutter/shell/platform/android/platform_view_android_unittests.cc
+++ engine/src/flutter/shell/platform/android/platform_view_android_unittests.cc
@@ -2,30 +2,93 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "gmock/gmock.h"
 #include "gtest/gtest.h"
 #include "shell/platform/android/flutter_main.h"
-#include "third_party/googletest/googlemock/include/gmock/gmock-nice-strict.h"
 
 namespace flutter {
 namespace testing {
 
-// TODO(matanlurey): Re-enable.
-//
-// This test (and the entire suite) was skipped on CI (see
-// https://github.com/flutter/flutter/issues/163742) and has since bit rotted
-// (we fallback to OpenGLES on emulators for performance reasons); either fix
-// the test, or remove it.
 #if !SLIMPELLER
-TEST(AndroidPlatformView, DISABLED_SelectsVulkanBasedOnApiLevel) {
+TEST(FlutterMainSelectedRenderingAPI, SelectsImpellerAutoselectOnApi29Plus) {
   Settings settings;
   settings.enable_software_rendering = false;
   settings.enable_impeller = true;
 
   EXPECT_EQ(FlutterMain::SelectedRenderingAPI(settings, 29),
-            AndroidRenderingAPI::kImpellerVulkan);
+            AndroidRenderingAPI::kImpellerAutoselect);
+  EXPECT_EQ(FlutterMain::SelectedRenderingAPI(settings, 34),
+            AndroidRenderingAPI::kImpellerAutoselect);
+}
+
+TEST(FlutterMainSelectedRenderingAPI, SelectsSkiaOpenGLESOnApiBelow29) {
+  Settings settings;
+  settings.enable_software_rendering = false;
+  settings.enable_impeller = true;
+
+  EXPECT_EQ(FlutterMain::SelectedRenderingAPI(settings, 28),
+            AndroidRenderingAPI::kSkiaOpenGLES);
   EXPECT_EQ(FlutterMain::SelectedRenderingAPI(settings, 24),
+            AndroidRenderingAPI::kSkiaOpenGLES);
+}
+
+TEST(FlutterMainSelectedRenderingAPI, SelectsSkiaOpenGLESWhenImpellerDisabledOnApi29Plus) {
+  Settings settings;
+  settings.enable_software_rendering = false;
+  settings.enable_impeller = false;
+
+  EXPECT_EQ(FlutterMain::SelectedRenderingAPI(settings, 29),
+            AndroidRenderingAPI::kSkiaOpenGLES);
+  EXPECT_EQ(FlutterMain::SelectedRenderingAPI(settings, 34),
+            AndroidRenderingAPI::kSkiaOpenGLES);
+}
+
+#ifndef FLUTTER_RELEASE
+TEST(FlutterMainSelectedRenderingAPI, SelectsExplicitImpellerBackendWhenRequested) {
+  Settings settings;
+  settings.enable_software_rendering = false;
+  settings.enable_impeller = true;
+
+  settings.requested_rendering_backend = "vulkan";
+  EXPECT_EQ(FlutterMain::SelectedRenderingAPI(settings, 29),
+            AndroidRenderingAPI::kImpellerVulkan);
+
+  settings.requested_rendering_backend = "opengles";
+  EXPECT_EQ(FlutterMain::SelectedRenderingAPI(settings, 29),
             AndroidRenderingAPI::kImpellerOpenGLES);
+}
+
+TEST(FlutterMainSelectedRenderingAPI, SelectsSkiaOpenGLESWhenBackendRequestedWithImpellerDisabled) {
+  Settings settings;
+  settings.enable_software_rendering = false;
+  settings.enable_impeller = false;
+
+  settings.requested_rendering_backend = "vulkan";
+  EXPECT_EQ(FlutterMain::SelectedRenderingAPI(settings, 29),
+            AndroidRenderingAPI::kSkiaOpenGLES);
+
+  settings.requested_rendering_backend = "opengles";
+  EXPECT_EQ(FlutterMain::SelectedRenderingAPI(settings, 29),
+            AndroidRenderingAPI::kSkiaOpenGLES);
+}
+
+TEST(FlutterMainSelectedRenderingAPI, SelectsAutoselectWhenUnrecognizedBackendRequested) {
+  Settings settings;
+  settings.enable_software_rendering = false;
+  settings.enable_impeller = true;
+
+  settings.requested_rendering_backend = "unknown_backend";
+  EXPECT_EQ(FlutterMain::SelectedRenderingAPI(settings, 29),
+            AndroidRenderingAPI::kImpellerAutoselect);
+}
+#endif  // !FLUTTER_RELEASE
+
+TEST(FlutterMainSelectedRenderingAPI, SelectsSoftwareRenderingWhenRequested) {
+  Settings settings;
+  settings.enable_software_rendering = true;
+  settings.enable_impeller = false;
+
+  EXPECT_EQ(FlutterMain::SelectedRenderingAPI(settings, 29),
+            AndroidRenderingAPI::kSoftware);
 }
 #endif  // !SLIMPELLER
 
